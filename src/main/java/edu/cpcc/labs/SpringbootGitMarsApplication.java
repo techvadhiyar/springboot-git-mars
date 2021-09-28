87838a534971a28c4ac03e9c28bf32ae5274a88a
cpccharry <harry.subramaniam@cpcc.edu>
2021-09-28 00:15:44 -0400

    Initial release.

---
+++ src/main/java/edu/cpcc/labs/SpringbootGitMarsApplication.java
@@ -0,0 +1,13 @@
+package edu.cpcc.labs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGitMarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGitMarsApplication.class, args);
+	}
+
+}
